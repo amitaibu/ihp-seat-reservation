096a97c3ff481eb5c8921a900f2f01a9bc2026ba
Amitai Burstein <amitai@gizra.com>
2021-10-14 16:24:01 +0300

    Initial commit and sata structure

---
+++ Application/Fixtures.sql
@@ -0,0 +1,51 @@
+
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
+SET SESSION AUTHORIZATION DEFAULT;
+
+ALTER TABLE public.libraries DISABLE TRIGGER ALL;
+
+
+
+ALTER TABLE public.libraries ENABLE TRIGGER ALL;
+
+
+ALTER TABLE public.library_openings DISABLE TRIGGER ALL;
+
+
+
+ALTER TABLE public.library_openings ENABLE TRIGGER ALL;
+
+
+ALTER TABLE public.reservations DISABLE TRIGGER ALL;
+
+
+
+ALTER TABLE public.reservations ENABLE TRIGGER ALL;
+
+
+ALTER TABLE public.seat_categories DISABLE TRIGGER ALL;
+
+
+
+ALTER TABLE public.seat_categories ENABLE TRIGGER ALL;
+
+
+ALTER TABLE public.users DISABLE TRIGGER ALL;
+
+
+
+ALTER TABLE public.users ENABLE TRIGGER ALL;
+
+
---
+++ Application/Schema.sql
@@ -0,0 +1,38 @@
+-- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
+CREATE TYPE reservation_status AS ENUM ('accepted', 'rejected', 'queued');
+CREATE TABLE users (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    email TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    locked_at TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+    failed_login_attempts INT DEFAULT 0 NOT NULL
+);
+CREATE TABLE libraries (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    title TEXT NOT NULL
+);
+CREATE TABLE reservations (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    library_opening_id UUID NOT NULL,
+    seat_number INT NOT NULL,
+    student_identifier TEXT NOT NULL
+);
+CREATE TABLE library_openings (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    start_time TIMESTAMP WITH TIME ZONE NOT NULL,
+    end_time TIMESTAMP WITH TIME ZONE NOT NULL,
+    library_id UUID NOT NULL
+);
+CREATE TABLE seat_categories (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    title TEXT NOT NULL,
+    from_number INT NOT NULL,
+    to_number INT NOT NULL
+);
+CREATE INDEX reservations_library_opening_id_index ON reservations (library_opening_id);
+CREATE INDEX library_openings_library_id_index ON library_openings (library_id);
+ALTER TABLE library_openings ADD CONSTRAINT library_openings_ref_library_id FOREIGN KEY (library_id) REFERENCES libraries (id) ON DELETE NO ACTION;
+ALTER TABLE reservations ADD CONSTRAINT reservations_ref_library_opening_id FOREIGN KEY (library_opening_id) REFERENCES library_openings (id) ON DELETE NO ACTION;
