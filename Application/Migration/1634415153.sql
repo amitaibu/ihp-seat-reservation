2f6ab2246df3170b1691403897d1ff637ff7b085
Amitai Burstein <amitai@gizra.com>
2021-10-17 00:00:22 +0300

    Add reservations delete

--- Application/Migration/1634415153.sql
+++ Application/Migration/1634415153.sql
@@ -3,5 +3,5 @@
 -- When you generate a new migration, all changes in this file will be copied into your migration.
 -- -- Use http://localhost:8001/NewMigration or `new-migration` to generate a new migration.
 -- -- Learn more about migrations: https://ihp.digitallyinduced.com/Guide/database-migrations.html
-ALTER TABLE reservation_jobs DROP CONSTRAINT;
+ALTER TABLE reservation_jobs DROP CONSTRAINT reservation_jobs_ref_reservation_id;
 ALTER TABLE reservation_jobs ADD CONSTRAINT reservation_jobs_ref_reservation_id FOREIGN KEY (reservation_id) REFERENCES reservations (id) ON DELETE CASCADE;
