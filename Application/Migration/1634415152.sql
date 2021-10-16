7a777ba3fbf7c590a0b2769b02cf034a04dfc937
Amitai Burstein <amitai@gizra.com>
2021-10-16 23:38:38 +0300

    Fix alter constraint

--- Application/Migration/1634415152.sql
+++ Application/Migration/1634415152.sql
@@ -3,5 +3,5 @@
 -- When you generate a new migration, all changes in this file will be copied into your migration.
 -- -- Use http://localhost:8001/NewMigration or `new-migration` to generate a new migration.
 -- -- Learn more about migrations: https://ihp.digitallyinduced.com/Guide/database-migrations.html
-ALTER TABLE seat_categories DROP CONSTRAINT seat_categories_ref_library_id
+ALTER TABLE seat_categories DROP CONSTRAINT seat_categories_ref_library_id;
 ALTER TABLE seat_categories ADD CONSTRAINT seat_categories_ref_library_id FOREIGN KEY (library_id) REFERENCES libraries (id) ON DELETE CASCADE;
